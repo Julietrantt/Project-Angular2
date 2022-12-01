2ace547c4de633bff72578112fd49b010855afea
Julietrantt <1995julietran@gmail.com>
2022-12-02 00:40:13 +0100

    adding users

--- my-app/src/app/components/users/users.component.ts
+++ my-app/src/app/components/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.css']
 })
 export class UsersComponent implements OnInit {
+  userList:any;
+  userList$:any;
 
-  constructor() { }
+  constructor(private usersService:UsersService) { }
 
   ngOnInit(): void {
+
   }
 
+  getUsers():void {
+    this.userList$ = this.usersService.getUsersList().pipe(tap((users)=>(this.userList = users)));
+  }
 }
